bc1f2fa9999e571f855d1a34890726a29a40e034
ToruNishioka <77736902+ToruNishioka@users.noreply.github.com>
2021-04-17 16:31:25 +0900

    ユーザー新規登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_005737) do
     t.string "first_name", null: false
     t.string "last_name_k", null: false
     t.string "first_name_k", null: false
-    t.date "birthdya", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
