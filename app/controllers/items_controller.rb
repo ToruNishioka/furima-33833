56775c6ff95c2d67c879ab1532c09da4f22a64e3
ToruNishioka <77736902+ToruNishioka@users.noreply.github.com>
2021-05-01 11:53:53 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :status_id, :delivery_charge_id, :area_id, :days_to_ship_id, :price, :user, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :item
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :area_id
+    validates :days_to_ship_id
+    validates :price
+    end
 end
