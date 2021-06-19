11c7b8fbf6dd0ad280659147b73bcf2a7918f4b4
ToruNishioka <77736902+ToruNishioka@users.noreply.github.com>
2021-06-19 17:43:58 +0900

    商品購入機能の修正6

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
   before do
-    # user = FactoryBot.create(:user) factoriesのitems.rbでuserとアソシエーションを組んでいるので不要
+    user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
-    @purchase_address = FactoryBot.build(:purchase_address, user_id: item.user.id, item_id: item.id)
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
     sleep 2
   end
 
